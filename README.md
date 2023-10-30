c16b28713846e494d88f982d49b6b21fe512dc8f
stepney141 <stepney141andi.c.i@gmail.com>
2023-10-30 17:24:05 +0900

    feat: 第1章

---
+++ README.md
@@ -0,0 +1,7 @@
+# 写経
+
+## 初知り事項メモ
+
+- Cargo.tomlのフィーチャーフラグ
+  - https://qiita.com/osanshouo/items/43271813b5d62e89d598
+- rustのタプルは `tuple.0` みたいにインデックスアクセスできる
---
+++ walk-the-dog/src/lib.rs
@@ -0,0 +1,103 @@
+use rand::prelude::*;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::console;
+
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+) {
+    let color_str = format!("rgb({},{},{})", color.0, color.1, color.2);
+    let [top, left, right] = points;
+
+    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+    context.stroke();
+    context.fill();
+}
+
+fn midpoint(point1: (f64, f64), point2: (f64, f64)) -> (f64, f64) {
+    ((point1.0 + point2.0) / 2.0, (point1.1 + point2.1) / 2.0)
+}
+
+fn sierpinski(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+    depth: u8,
+) {
+    draw_triangle(&context, points, color);
+
+    let depth = depth - 1;
+    let [top, left, right] = points;
+    let mut rng = thread_rng();
+    let next_color = (
+        rng.gen_range(0..255),
+        rng.gen_range(0..255),
+        rng.gen_range(0..255),
+    );
+
+    if depth > 0 {
+        let left_middle = midpoint(top, left);
+        let right_middle = midpoint(top, right);
+        let bottom_middle = midpoint(left, right);
+        sierpinski(
+            &context,
+            [top, left_middle, right_middle],
+            next_color,
+            depth,
+        );
+        sierpinski(
+            &context,
+            [left_middle, left, bottom_middle],
+            next_color,
+            depth,
+        );
+        sierpinski(
+            &context,
+            [right_middle, bottom_middle, right],
+            next_color,
+            depth,
+        );
+    }
+}
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
+    // Your code goes here!
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    sierpinski(
+        &context,
+        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+        (200, 10, 10),
+        7,
+    );
+
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
+}
